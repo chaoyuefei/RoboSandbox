4a49159451282b27ec9106593a1072c926e48eb9
chaoyuefei <chaoyuefei@outlook.com>
2025-01-21 15:06:56 +0100

    feat: clean tutorial and add optimization feature

--- build/lib/robosandbox/__init__.py
+++ build/lib/robosandbox/__init__.py
@@ -1,4 +1,6 @@
 from robosandbox import models
 from robosandbox import performance
+# from robosandbox import optimization
 
+# __all__ = ["models", "performance", "optimization"]
 __all__ = ["models", "performance"]
--- robosandbox/__init__.py
+++ robosandbox/__init__.py
@@ -1,5 +1,6 @@
 from robosandbox import models
 from robosandbox import performance
-from robosandbox import optimization
+# from robosandbox import optimization
 
-__all__ = ["models", "performance", "optimization"]
+# __all__ = ["models", "performance", "optimization"]
+__all__ = ["models", "performance"]
