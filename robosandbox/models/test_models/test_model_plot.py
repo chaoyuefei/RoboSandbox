8bc20b86907675a4fdc6f031525be4d5656d6436
chaoyue <chaoyuefei@outlook.com>
2025-02-19 09:57:34 +0100

    test connection

--- robosandbox/models/test_models/test_model_plot.py
+++ robosandbox/models/test_models/test_model_plot.py
@@ -1,10 +1,14 @@
 import robosandbox as rsb
 
+
 def test_GenericFour():
     robot = rsb.models.DH.Generic.GenericFour()
     robot.plot(robot.qz)
+    tfs = robot.fkine_all(robot.qz)
+    print(tfs)
     assert robot is not None, "GenericFour robot not defined"
     assert robot.n == 4, "GenericFour robot has 4 joints"
+
 
 if __name__ == "__main__":
     test_GenericFour()
