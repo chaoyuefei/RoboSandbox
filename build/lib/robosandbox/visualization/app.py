dffcf7d3ed8d90a1c3cc138f1f541960de853f4a
chaoyuefei <chaoyuefei@outlook.com>
2025-03-04 15:49:07 +0100

    fix: better app layout

--- build/lib/robosandbox/visualization/app.py
+++ build/lib/robosandbox/visualization/app.py
@@ -12,9 +12,13 @@ app.layout = dbc.Container(
     [
         dbc.Row(
             [
+                html.H2("Robot Arm Design App"),
+                html.H5("@Chaoyue Fei"),
+                html.Div(style={"height": "30px"}),
+                html.Hr(),
                 dbc.Col(
                     [
-                        html.H2("Robot Arm Design App"),
+                        # html.H2("Robot Arm Design App"),
                         # Key Parameters 区域
                         html.Div(
                             [
@@ -71,7 +75,7 @@ app.layout = dbc.Container(
                     [
                         html.H5("Robot Arm Configuration"),
                         dbc.Spinner(
-                            dcc.Graph(id="arm_display", style={"height": "80vh"}),
+                            dcc.Graph(id="arm_display", style={"height": "75vh"}),
                             color="primary",
                         ),
                         html.Div(id="output", style={"margin-top": "20px"}),
@@ -103,7 +107,6 @@ def update_robot_arm(n_clicks, dofs, link_lengths, alpha, qs):
     if n_clicks is None:
         return {}, "Please click the button to generate the robot arm"
 
-    # 解析用户输入的连杆长度和攻角
     try:
         link_lengths = list(map(float, link_lengths.split(",")))
         alpha = list(map(float, alpha.split(",")))
@@ -111,23 +114,14 @@ def update_robot_arm(n_clicks, dofs, link_lengths, alpha, qs):
     except ValueError:
         return {}, "Please enter valid numbers for link lengths and alpha angles."
 
-    # 根据 DOFs 绘制不同数量的圆
     fig = go.Figure()
 
     if dofs == 2:
-        # 绘制两个圆
-        for i in range(2):
-            fig.add_shape(
-                type="circle",
-                xref="x",
-                yref="y",
-                x0=i,
-                y0=0,
-                x1=i + 1,
-                y1=1,
-                fillcolor="skyblue",
-                line_color="blue",
-            )
+        robot = rsb.models.DH.Generic.GenericTwo(
+            linklengths=link_lengths, alpha=[np.deg2rad(a) for a in alpha]
+        )
+        robot.plotly(np.deg2rad(qs), isShow=False, fig=fig)
+
     elif dofs == 3:
         robot = rsb.models.DH.Generic.GenericThree(
             linklengths=link_lengths, alpha=[np.deg2rad(a) for a in alpha]
@@ -140,11 +134,10 @@ def update_robot_arm(n_clicks, dofs, link_lengths, alpha, qs):
         )
         robot.plotly(np.deg2rad(qs), isShow=False, fig=fig)
 
-    # 设置图形布局
     fig.update_layout(
         # title=f"Robot Arm with {dofs} DOFs",
-        xaxis=dict(title="X", range=[-1, 4]),
-        yaxis=dict(title="Y", range=[-1, 2]),
+        # xaxis=dict(title="X", range=[-1, 4]),
+        # yaxis=dict(title="Y", range=[-1, 2]),
         showlegend=True,
     )
 
--- robosandbox/visualization/app.py
+++ robosandbox/visualization/app.py
@@ -12,9 +12,13 @@ app.layout = dbc.Container(
     [
         dbc.Row(
             [
+                html.H2("Robot Arm Design App"),
+                html.H5("@Chaoyue Fei"),
+                html.Div(style={"height": "30px"}),
+                html.Hr(),
                 dbc.Col(
                     [
-                        html.H2("Robot Arm Design App"),
+                        # html.H2("Robot Arm Design App"),
                         # Key Parameters 区域
                         html.Div(
                             [
@@ -71,7 +75,7 @@ app.layout = dbc.Container(
                     [
                         html.H5("Robot Arm Configuration"),
                         dbc.Spinner(
-                            dcc.Graph(id="arm_display", style={"height": "80vh"}),
+                            dcc.Graph(id="arm_display", style={"height": "75vh"}),
                             color="primary",
                         ),
                         html.Div(id="output", style={"margin-top": "20px"}),
@@ -103,7 +107,6 @@ def update_robot_arm(n_clicks, dofs, link_lengths, alpha, qs):
     if n_clicks is None:
         return {}, "Please click the button to generate the robot arm"
 
-    # 解析用户输入的连杆长度和攻角
     try:
         link_lengths = list(map(float, link_lengths.split(",")))
         alpha = list(map(float, alpha.split(",")))
@@ -111,7 +114,6 @@ def update_robot_arm(n_clicks, dofs, link_lengths, alpha, qs):
     except ValueError:
         return {}, "Please enter valid numbers for link lengths and alpha angles."
 
-    # 根据 DOFs 绘制不同数量的圆
     fig = go.Figure()
 
     if dofs == 2:
@@ -132,11 +134,10 @@ def update_robot_arm(n_clicks, dofs, link_lengths, alpha, qs):
         )
         robot.plotly(np.deg2rad(qs), isShow=False, fig=fig)
 
-    # 设置图形布局
     fig.update_layout(
         # title=f"Robot Arm with {dofs} DOFs",
-        xaxis=dict(title="X", range=[-1, 4]),
-        yaxis=dict(title="Y", range=[-1, 2]),
+        # xaxis=dict(title="X", range=[-1, 4]),
+        # yaxis=dict(title="Y", range=[-1, 2]),
         showlegend=True,
     )
 
