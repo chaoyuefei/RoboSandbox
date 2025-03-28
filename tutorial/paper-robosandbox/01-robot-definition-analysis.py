6966cc0d5087596d1d58bb9e7b66109d03099d23
chaoyuefei <chaoyuefei@outlook.com>
2025-03-28 12:48:38 +0100

    tutorial: paper 01

---
+++ tutorial/paper-robosandbox/01-robot-definition-analysis.py
@@ -0,0 +1,128 @@
+import marimo
+
+__generated_with = "0.11.30"
+app = marimo.App()
+
+
+@app.cell
+def _():
+    import robosandbox as rsb
+    import numpy as np
+    import plotly.graph_objects as go # import new fig env
+
+    return go, np, rsb
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        # Models
+
+        The "models" subpackage offers a variety of robotic manipulator models that use different description methodologies, such as DH parameters and screw theory. Currently, all the models can be classified into three categories:
+
+        - DH: created using Denavit-Hartenberg Parameters, for example: ro⊥=rsb.⊨.DH.Ge≠ric.Ge≠ricFour()robot = rsb.models.DH.Generic.GenericFour(), powered by (roboticstoolbox-python)[https://github.com/petercorke/robotics-toolbox-python]
+        - DHLink: created using Denavit-Hartenberg Parameters, taking into consideration the design of the links.
+        - MR: created using screw theory, based on (Modern Robotics)[https://hades.mech.northwestern.edu/index.php/Modern_Robotics] and (code)[https://github.com/NxRLab/ModernRobotics]
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ## DH Models
+
+        ### Generic Models
+        We can easily define a robotic manipulate using DH parameters in RoboSandbox. To give a quick start, several pre-defined models are offered from 2 Dofs to 7 Dofs:
+        """
+    )
+    return
+
+
+@app.cell
+def _(go):
+    import robosandbox.models.DH.Generic as generic
+    robot4 = generic.GenericFour()
+    robot4.plotly(robot4.qz, fig=go.Figure()) # In the Marimo notebook, use the "fig" parameter for improved visual effects.
+    return generic, robot4
+
+
+@app.cell
+def _(generic, go):
+    robot6 = generic.GenericSix()
+    robot6.plotly(robot6.qz, fig=go.Figure())
+    return (robot6,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        """
+        ### Commercial Models
+
+        In addition to generic models, several commercial robots are available, such as Panda, Puma 560, and UR5.
+        """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""#### Panda""")
+    return
+
+
+@app.cell
+def _(go, rsb):
+    Panda = rsb.models.DH.Panda()
+    Panda.plotly(Panda.qr, fig=go.Figure())
+    return (Panda,)
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""#### Puma 560""")
+    return
+
+
+@app.cell
+def _(go, rsb):
+    Puma560 = rsb.models.DH.Puma560()
+    Puma560.plotly(Puma560.qr, fig=go.Figure())
+    return (Puma560,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+        ### Make a change
+
+        Beginning with the default settings, we can easily adjust the robot's link length and actuator directions. These are important design parameters that we will explore further in the optimization chapter.
+        """
+    )
+    return
+
+
+@app.cell
+def _(generic, go, np):
+    new_robot = generic.GenericFour(
+                    linklengths=[0.5, 0.5, 0.5, 0.5], 
+                    alpha=[0, np.pi/2, np.pi/2, 0],
+                )
+
+    new_robot.plotly(new_robot.qz, save=False, fig=go.Figure())
+    return (new_robot,)
+
+
+if __name__ == "__main__":
+    app.run()
