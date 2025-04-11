938e316edafbd20b1fef590628bc9333c9df845e
Chaoyue Fei <chaoyue.fei@vub.be>
2025-04-11 11:31:33 +0200

    feat: add swift traj

--- robosandbox/models/URDF/new_robot.py
+++ robosandbox/models/URDF/new_robot.py
@@ -28,12 +28,33 @@ class New(ERobot):
 
 
 if __name__ == "__main__":  # pragma nocover
+    from roboticstoolbox import jtraj
+    import swift
+
     r = New()
 
     r.qz
-    print(r)
+    # print(r)
     # r.plot(q=[0.2, -2, -0.3, 0.3, 0.4, 0.5], backend="swift", block=True)
     # r.plot(q=r.qz, backend="swift", block=True)
-    print(r.fkine(r.qz))
-    print(r.links[2])
-    r.plot(q=[0, 0, 0, 0], backend="swift", block=True)
+    # print(r.fkine(r.qz))
+    # print(r.links[2])
+    q0 = r.qz
+    qe = np.array([0.7, -1, 0, 2.2])
+    qtraj = jtraj(q0, qe, 100)
+    # print(qtraj.q)
+    # env = swift.Swift()
+    # env.launch(realtime=True)
+    # env.add(r)
+    # r.plot(q=qtraj.q, backend="swift")
+    #
+    # URDF + Swift version
+    dt = 0.050  # simulation timestep in seconds
+
+    env = swift.Swift()  # instantiate 3D browser-based visualizer       # activate it
+    env.launch(realtime=True)
+    env.add(r)  # add robot to the 3D scene
+    for qk in qtraj.q:  # for each joint configuration on trajectory
+        r.q = qk  # update the robot state
+        env.step()  # update visualization
+    # env.stop_recording()
