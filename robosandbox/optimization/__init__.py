27a6302de27f142bb9c40baba4314f1de6bd71d1
Chaoyue Fei <chaoyue.fei@vub.be>
2025-05-08 13:35:03 +0200

    clean the code

--- robosandbox/optimization/__init__.py
+++ robosandbox/optimization/__init__.py
@@ -1,5 +1,4 @@
-from robosandbox.optimization import opti_scipy
 from robosandbox.optimization import sweeper
 from robosandbox.optimization import problem
 
-__all__ = ["opti_scipy", "sweeper", "problem"]
+__all__ = ["sweeper", "problem"]
