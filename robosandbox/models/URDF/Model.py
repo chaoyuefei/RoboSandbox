c6b7bb22356178547df046e64826c547467d8dda
Chaoyue Fei <chaoyue.fei@vub.be>
2025-06-19 14:32:04 +0200

    feat: read urdf

--- robosandbox/models/URDF/Model.py
+++ robosandbox/models/URDF/Model.py
@@ -8,11 +8,34 @@ from spatialmath import SE3
 class Model(Robot):
     """
     Class that imports a URDF model from file.
+    file_path: str
+        Path to the URDF file.
+    tld: str
+        Top-level directory for the URDF file, used for resolving relative paths.
+    xacro_tld: str
+        Top-level directory for XACRO files, used for resolving relative paths in XACRO files.
+    name: str
+        Name of the robot model.
+    manufacturer: str
+        Manufacturer of the robot model.
+    gripper_links: list
+        List of gripper links, if any. If not provided, defaults to an empty list.
+    This class reads the URDF file, extracts the links, and initializes the robot model.
     """
 
-    def __init__(self, name, manufacturer, gripper_links, path):
+    def __init__(
+        self,
+        file_path: str,
+        tld: str = "",
+        xacro_tld: str = "",
+        name: str = "",
+        manufacturer: str = "",
+        gripper_links=None,
+    ):
         links, name, urdf_string, urdf_filepath = self.URDF_read(
-            path,
+            file_path=file_path,
+            tld=tld,
+            xacro_tld=xacro_tld,
         )
 
         super().__init__(
@@ -31,10 +54,12 @@ class Model(Robot):
         # )
 
 
-# if __name__ == "__main__":  # pragma nocover
-#     r = Model()
-
-#     r.qz
-
-#     for link in r.grippers[0].links:
-#         print(link)
+if __name__ == "__main__":  # pragma nocover
+    # SO101 = Model(
+    #     name="SO101",
+    #     file_path="so100.urdf",
+    #     tld="/Users/chaoyue/Documents/coding/RoboSandbox/robosandbox/models/URDF/data/SO100",
+    # )
+    # SO101.q = np.zeros(6)
+    # print(SO101)
+    # SO101.plot(q=SO101.q)
